c8f7e72ea0b0a7df38f5cfe23a60c7e623d3890b
emurze <grayemurze@gmail.com>
2024-02-08 05:51:46 +0300

    DDD

--- src/shared/infra/sqlalchemy_orm/base.py
+++ src/shared/infra/sqlalchemy_orm/base.py
@@ -1,7 +1,7 @@
 from shared.infra.sqlalchemy_orm.common.base import Base
-from shared.infra.sqlalchemy_orm.utils.import_contracts import import_contracts
+from shared.infra.sqlalchemy_orm.common import import_contracts
 from shared.infra.sqlalchemy_orm.config.base import PATH, FILENAME
 
-contracts = import_contracts(PATH, FILENAME)
+contracts = import_contracts.import_contracts(PATH, FILENAME)
 
 base = Base(contracts)
