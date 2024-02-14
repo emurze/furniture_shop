8de71195625918372a14f98ca2f91dbdb886cef6
emurze <grayemurze@gmail.com>
2024-02-14 07:38:47 +0300

    feature(e2e in main.yml)

--- tests/slow/e2e/conftest.py
+++ tests/slow/e2e/conftest.py
@@ -1,7 +1,21 @@
+from collections.abc import AsyncIterator
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from shared.infra.sqlalchemy_orm.db import get_session
 
 from api.main import app
+from tests.slow.conftest import async_session_maker
+
+
+async def get_test_session() -> AsyncIterator[AsyncSession]:
+    async with async_session_maker() as session:
+        yield session
+
+
+app.dependency_overrides[get_session] = get_test_session
 
 
 @pytest.fixture
